e9ca2fda1a79d67d68bd8345dc28378f927128ce
Adrián Martínez Roca <amartinez@gregal.info>
2025-11-19 10:56:53 +0100

    Ejercicios 1-2 - Tema 13

---
+++ tema13-ej1/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tema13-ej1',
+  webDir: 'www'
+};
+
+export default config;
